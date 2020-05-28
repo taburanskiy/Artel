946a73ebd32434a7c217fb18303d21f679360743
taburanskiy <31410232+taburanskiy@users.noreply.github.com>
2020-05-28 19:07:02 +0300

    Add files via upload

--- example.py
+++ example.py
@@ -1,28 +1,41 @@
 from random import randint
+
 combo_num = 0
+sequence = False
 
 combo = {
     1: 'Bingo',
     2: 'Combo',
     3: 'Casino',
     4: 'Cheater',
-    5: 'Royal Flash'  
+    5: 'Royal Flash'
 }
 
 def combinator(value):
   return combo.get(value)
- 
-sequence = False
+
+def input_number():
+    try:
+        num = int(input('Веддіть число '))
+        if num >0 and num <=3:
+            return num
+        else: 
+            print("Число не в діапазоні від 1 до 3")
+    except ValueError:
+        print('Введено не число!!!!')
+        input_number()
+
+
 while True:
   x = randint(1,3)
   print('Число загадане')
   while True:
-    num =int(input('Веддіть число'))
+    num = input_number()
     if num == x :
-      if sequence == True: 
+      if sequence == True:
         combo_num += 1
         print(combinator(combo_num))
-      else: 
+      else:
         sequence = True
         combo_num = 1
         print(combinator(combo_num))
@@ -30,3 +43,4 @@ while True:
     else:
       sequence = False
       continue
+
